8899833b409b74a80097518eeb49f6ecd8f44332
Dandee Galang <dandeegalang.ph@gmail.com>
2025-02-04 11:19:47 +0800

    added dependencies

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
+#include "raylib.h"
+
+#if defined(_WIN32)           
+	#define NOGDI            
+	#define NOUSER           
+#endif
+
+#include <Windows.h> 
+
+#if defined(_WIN32)           
+	#undef far
+#endif
+
+#include "cpr/cpr.h"
+#include "json.hpp"
 #include <iostream>
 
+using json = nlohmann::json;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "AirplanesLive Clone Test");
+
+
+    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/raysan5/raylib"});
+    
+
+    if (r.status_code == 200) {
+        json j = json::parse(r.text);
+        std::string repoName = j["full_name"];
+        std::string repoDescription = j["description"];
+
+        std::cout << "Repository: " << repoName << std::endl;
+        std::cout << "Description: " << repoDescription << std::endl;
+    } else {
+        std::cerr << "Failed to fetch data from GitHub API. Status code: " << r.status_code << std::endl;
+    }
+
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+  
+    CloseWindow();
+
     return 0;
 }
